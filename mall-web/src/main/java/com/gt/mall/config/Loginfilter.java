b9e5e10efb1e38a556ef33539ace5622e1b3b463
chendan <843964653@qq.com>
2017-07-24 20:12:17 +0800

    拦截器提交一下

---
+++ mall-web/src/main/java/com/gt/mall/config/Loginfilter.java
@@ -0,0 +1,18 @@
+package com.gt.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * 添加拦截器demo
+ */
+@Configuration
+public class Loginfilter extends WebMvcConfigurerAdapter {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new MyInterceptor()).addPathPatterns("/**");
+
+        super.addInterceptors(registry);
+    }
+}
---
+++ mall-web/src/main/java/com/gt/mall/config/MyInterceptor.java
@@ -0,0 +1,30 @@
+package com.gt.mall.config;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by Administrator on 2017/7/24 0024.
+ */
+public class MyInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws Exception {
+        System.out.println(">>>MyInterceptor1>>>>>>>在请求处理之前进行调用（Controller方法调用之前）");
+
+        return true;// 只有返回true才会继续向下执行，返回false取消当前请求
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+            throws Exception {
+    }
+}
