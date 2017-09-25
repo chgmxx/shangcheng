ccf4990cd4dd10b17994e35030e5c1266f4d3f3d
yangqian <3001372437@qq.com>
2017-09-25 14:34:56 +0800

    跨域配置

--- mall-web/src/main/java/com/gt/mall/config/interceptor/Loginfilter.java
+++ mall-web/src/main/java/com/gt/mall/config/interceptor/Loginfilter.java
@@ -1,6 +1,7 @@
 package com.gt.mall.config.interceptor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -15,5 +16,18 @@ public class Loginfilter extends WebMvcConfigurerAdapter {
 	//        registry.addInterceptor(new SysLogInterceptor()).addPathPatterns("/**");
 
 	super.addInterceptors( registry );
+    }
+
+    /**
+     * 跨域配置
+     * 默认设置全局跨域配置
+     * TODO: 部署服务器需要注释掉。因为，nginx已配置跨域。否则会起冲突
+     *
+     * @param registry Corsregistry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+	registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+	super.addCorsMappings(registry);
     }
 }
