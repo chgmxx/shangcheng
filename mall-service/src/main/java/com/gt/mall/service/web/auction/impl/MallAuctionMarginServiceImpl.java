5ee912a0a0cfce29ed2677728082105a060858f0
yangqian <3001372437@qq.com>
2018-01-08 11:12:13 +0800

    new

--- mall-service/src/main/java/com/gt/mall/service/web/auction/impl/MallAuctionMarginServiceImpl.java
+++ mall-service/src/main/java/com/gt/mall/service/web/auction/impl/MallAuctionMarginServiceImpl.java
@@ -274,11 +274,13 @@ public class MallAuctionMarginServiceImpl extends BaseServiceImpl< MallAuctionMa
 	subQrPayParams.setMemberId( member.getId() );//会员id
 	subQrPayParams.setDesc( "商城缴纳拍卖定金" );//描述
 	subQrPayParams.setIsreturn( 1 );//是否需要同步回调(支付成功后页面跳转),1:需要(returnUrl比传),0:不需要(为0时returnUrl不用传)
-	subQrPayParams.setReturnUrl( PropertiesUtil.getHomeUrl() + "/phoneAuction/L6tgXlBFeK/myMargin.do" );
+	subQrPayParams.setReturnUrl(
+			PropertiesUtil.getPhoneWebHomeUrl() + "/auction/success/" + member.getBusid() + "/" + mallAuctionMargin.getProId() + "/" + mallAuctionMargin.getAucId()
+					+ "/" + mallAuctionMargin.getShopId() +"/4");
 	subQrPayParams.setNotifyUrl( PropertiesUtil.getHomeUrl()
-			+ "/phoneAuction/L6tgXlBFeK/payWay.do" );//异步回调，注：1、会传out_trade_no--订单号,payType--支付类型(0:微信，1：支付宝2：多粉钱包),2接收到请求处理完成后，必须返回回调结果：code(0:成功,-1:失败),msg(处理结果,如:成功)
-	subQrPayParams.setIsSendMessage( 1 );//是否需要消息推送,1:需要(sendUrl比传),0:不需要(为0时sendUrl不用传)
-	subQrPayParams.setSendUrl( PropertiesUtil.getHomeUrl() + "/mAuction/margin.do" );//推送路径(尽量不要带参数)
+			+ "phoneAuction/L6tgXlBFeK/payWay.do" );//异步回调，注：1、会传out_trade_no--订单号,payType--支付类型(0:微信，1：支付宝2：多粉钱包),2接收到请求处理完成后，必须返回回调结果：code(0:成功,-1:失败),msg(处理结果,如:成功)
+	subQrPayParams.setIsSendMessage( 0 );//是否需要消息推送,1:需要(sendUrl比传),0:不需要(为0时sendUrl不用传)
+	//	subQrPayParams.setSendUrl( PropertiesUtil.getHomeUrl() + "mAuction/margin.do" );//推送路径(尽量不要带参数)
 	int payWay = 1;
 	if ( mallAuctionMargin.getPayWay() == 3 ) {//支付宝支付
 	    payWay = 2;
