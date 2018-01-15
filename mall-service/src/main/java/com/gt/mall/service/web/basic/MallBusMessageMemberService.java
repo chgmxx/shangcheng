160fe2a74e024666a3187608ded2b33f7958740e
maoyl <3001321595@qq.com>
2018-01-15 10:04:44 +0800

    update

--- mall-service/src/main/java/com/gt/mall/service/web/basic/MallBusMessageMemberService.java
+++ mall-service/src/main/java/com/gt/mall/service/web/basic/MallBusMessageMemberService.java
@@ -26,16 +26,16 @@ public interface MallBusMessageMemberService extends BaseService< MallBusMessage
      *
      * @return
      */
-    List<MallBusMessageMember> selectByBusId( Integer busId );
+    List< MallBusMessageMember > selectByBusId( Integer busId );
 
     /**
      * 买家付款成功通知商家
      *
      * @param order
-     * @param busUser
+     * @param member
      * @param type    发送类型 0全部 1短信 2模板
      */
-    void buyerPaySuccess( MallOrder order, BusUser busUser, Integer type,String telePhone );
+    void buyerPaySuccess( MallOrder order, Member member, Integer type, String telePhone );
 
     /**
      * 买家确认收货通知商家
@@ -49,8 +49,16 @@ public interface MallBusMessageMemberService extends BaseService< MallBusMessage
      * 买家订单维权通知商家
      *
      * @param orderReturn
-     * @param busId
+     * @param member
      * @param type        发送类型 0全部 1短信 2模板
      */
-    void buyerOrderReturn( MallOrderReturn orderReturn, Integer busId, Integer type );
+    void buyerOrderReturn( MallOrderReturn orderReturn, Member member, Integer type );
+
+    /**
+     * 粉丝申请成为超级销售员 通知商家
+     *
+     * @param member   粉丝
+     * @param telPhone 接收申请审核手机
+     */
+    void memberApplySeller( Member member, String telPhone);
 }
